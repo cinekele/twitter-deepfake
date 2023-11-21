b712d394f12d8645580af2d45e69434ef46dfb7a
cinekele <cinekele@gmail.com>
2023-11-21 07:39:05 +0100

    modify modeling

--- notebooks/utils/optuna_utils.py
+++ notebooks/utils/optuna_utils.py
@@ -5,7 +5,7 @@ from optuna import Trial
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import balanced_accuracy_score, make_scorer
+from sklearn.metrics import balanced_accuracy_score, make_scorer, f1_score, precision_score, recall_score
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 from sklearn.pipeline import Pipeline
 from sklearn.svm import SVC
@@ -64,14 +64,16 @@ def instantiate_lr(trial: Trial) -> LogisticRegression:
     return LogisticRegression(**params)
 
 
+NGRAMS_MAPPING = {
+    "unigram": (1, 1),
+    "digram": (1, 2),
+    "trigram": (1, 3)
+}
+
+
 def instantiate_tfidf(trial: Trial) -> TfidfVectorizer:
     ngram = trial.suggest_categorical('tfidf_ngram_range', ["unigram", "digram", "trigram"])
-    if ngram == "unigram":
-        ngram_range = (1, 1)
-    elif ngram == "digram":
-        ngram_range = (1, 2)
-    else:
-        ngram_range = (1, 3)
+    ngram_range = NGRAMS_MAPPING[ngram]
     params = {
         "max_features": trial.suggest_int('tfidf_max_features', 1000, 10000, log=True),
         "ngram_range": ngram_range,
@@ -81,7 +83,7 @@ def instantiate_tfidf(trial: Trial) -> TfidfVectorizer:
     return TfidfVectorizer(**params)
 
 
-MODELS = {
+MODELS_MAPPING = {
     'LGBM': instantiate_lgbm,
     'XGB': instantiate_xgb,
     'RF': instantiate_rf,
@@ -89,15 +91,70 @@ MODELS = {
     'LR': instantiate_lr
 }
 
-ENCODERS = {
+ENCODERS_MAPPING = {
     'TFIDF': instantiate_tfidf
 }
+
+MODELS = {
+    'LGBM': LGBMClassifier,
+    'XGB': XGBClassifier,
+    'RF': RandomForestClassifier,
+    'SVC': SVC,
+    'LR': LogisticRegression
+}
+
+ENCODERS = {
+    'TFIDF': TfidfVectorizer
+}
+
+
+def extract_model_encoder(params: dict) -> (str, str, dict, dict):
+    model_params = {}
+    encoder_params = {}
+    for key in params.keys():
+        begin, end = key.split('_', 1)
+        begin = begin.upper()
+        if begin in MODELS.keys():
+            model = begin
+            model_params[end] = params[key]
+        elif begin in ENCODERS.keys():
+            encoder = begin
+            encoder_params[end] = params[key]
+    if encoder is None:
+        return model, model_params
+    return model, model_params, encoder, encoder_params
+
+
+def get_best_model(params: dict, X: pd.DataFrame, y: pd.Series) -> Pipeline:
+    model, model_params, encoder, encoder_params = extract_model_encoder(params)
+    type_of_model = MODELS[model.upper()]
+    if model in ('LGBM', 'XGB', 'RF', 'LR'):
+        model_params['n_jobs'] = 6
+    if model == 'LR':
+        model_params['solver'] = 'saga'
+        model_params['max_iter'] = 1000
+    model = type_of_model(**model_params)
+    if encoder is not None:
+        type_of_encoder = ENCODERS[encoder.upper()]
+        if "ngram_range" in encoder_params:
+            encoder_params["ngram_range"] = NGRAMS_MAPPING[encoder_params["ngram_range"]]
+        encoder = type_of_encoder(**encoder_params)
+        pipeline = Pipeline([
+            ('encoder', encoder),
+            ('model', model)
+        ])
+    else:
+        pipeline = Pipeline([
+            ('model', model)
+        ])
+    pipeline.fit(X, y)
+    return pipeline
 
 
 def objective(trial: Trial, X: pd.DataFrame, y: pd.Series, n_splits: int, model: str, encoder: str,
               random_state: int = 42) -> float:
-    type_of_model = MODELS[model]
-    type_of_encoder = ENCODERS[encoder]
+    type_of_model = MODELS_MAPPING[model]
+    type_of_encoder = ENCODERS_MAPPING[encoder]
     skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=random_state)
     model = type_of_model(trial)
     encoder = type_of_encoder(trial)
@@ -109,3 +166,14 @@ def objective(trial: Trial, X: pd.DataFrame, y: pd.Series, n_splits: int, model:
     scores = cross_val_score(pipeline, X, y, scoring=scorer, cv=skf)
 
     return np.mean(scores)
+
+
+def get_score(retrained_model, x_valid, y_valid):
+    y_pred = retrained_model.predict(x_valid)
+    results = {
+        'balanced_accuracy': balanced_accuracy_score(y_valid, y_pred),
+        'f1_score': f1_score(y_valid, y_pred),
+        'precision': precision_score(y_valid, y_pred),
+        'recall': recall_score(y_valid, y_pred)
+    }
+    return results
